aef98d876b1292704a47ef57673b85ab6464e5b8
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-11-28 18:24:50 +0800

    commit

--- Program2(PLD7).py
+++ Program2(PLD7).py
@@ -10,27 +10,3 @@
 #ouput: Valid
 
 #Tip: loop through each character of the input then process it letter by letter
-def vowel_count(str):
-      
-    # Initializing count variable to 0
-    count = 0
-      
-    # Creating a set of vowels
-    vowel = set("aeiouAEIOU")
-      
-    # Loop to traverse the alphabet
-    # in the given string
-    for alphabet in str:
-      
-        # If alphabet is present
-        # in set vowel
-        if alphabet in vowel:
-            count = count + 1
-      
-    print("No. of vowels :", count)
-      
-# Driver code 
-str = "jhed aso pogi gwapo"
-  
-# Function Call
-vowel_count(str)
