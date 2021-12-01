27517c6c1d00c045bba646f84d62fc6541134ee6
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-12-01 15:59:37 +0800

    commit

--- Program2(PLD7).py
+++ Program2(PLD7).py
@@ -12,26 +12,26 @@
 #Tip: loop through each character of the input then process it letter by letter
 
 print(">>\33[32;1mPASSWORD VALIDATOR\33[0m<<")
-user = input("\n\33[33;1mEnter your \33[32;1mPASSWORD\33[0m: ")
+user = input("\n\33[33;3mEnter your\33[0m \33[32;1mPASSWORD\33[0m: ")
 
 def validator():
     spclChar = ("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ".", "|", ";", ":", "'", "?", ",", "/", "<", ">", "~", "`")
     val = True
 
     if len(user) < 16:
-        print("\33[31;1mPassword length should be Greater than 15 letters.")
+        print("\33[31;3mPassword length should be Greater than 15 letters.")
         val = False
     
     if not any(char.isdigit() for char in user):
-        print("\33[31;1mPassword should have at least one number.")
+        print("\33[31;3mPassword should have at least one number.")
         val = False
     
     if not any(char.isupper() for char in user):
-        print("\33[31;1mPassword should have at least one capital letter.")
+        print("\33[31;3mPassword should have at least one capital letter.")
         val = False
 
     if not any(char in spclChar for char in user):
-        print("\33[31;1mPassword should have at least one special character.\33[0m")
+        print("\33[31;3mPassword should have at least one special character.\33[0m")
         val = False
     
     return val
